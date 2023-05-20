af1ff5b75782ccedcc12d79c7a12e32933cf9f4b
mollinaca <mail@mollinaca.dev>
2023-05-20 23:32:35 +0900

    update

--- flyers3/flyers3.py
+++ flyers3/flyers3.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+class Foo:
+
+    def say(self):
+        print ('Hi')
+        return 'hi'
+
 def main():
-    print ("main")
+    Foo().say()
     exit (0)
 
 
