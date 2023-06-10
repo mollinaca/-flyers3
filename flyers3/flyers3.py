c5afef23e604366e84124432556f1a3bfaf1c6f3
mollinaca <mail@mollinaca.dev>
2023-06-10 23:26:50 +0900

    add

--- flyers3/flyers3.py
+++ flyers3/flyers3.py
@@ -1,14 +1,55 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+import configparser
+import json
+import os
+import pathlib
+import requests
+import shutil
+import time
 
-class Foo:
+class F3:
 
-    def say(self):
-        print ('Hi')
-        return 'hi'
+    def get_slack_conf() -> list:
+        here = pathlib.Path(__file__).resolve().parent
+        # CONFIG_FILE = str(here) + "/config.ini"
+        CONFIG_FILE = str(here) + "/config_dev.ini"  # for development
+        cfg = configparser.ConfigParser()
+        cfg.read(CONFIG_FILE)
+        return ([cfg["slack"]["token"], cfg["slack"]["channel"]])
+
+    def get_target_shops() -> dict:
+        here = pathlib.Path(__file__).resolve().parent
+        # CONFIG_FILE = str(here) + "/config.ini"
+        CONFIG_FILE = str(here) + "/config_dev.ini"  # for development
+        cfg = configparser.ConfigParser()
+        cfg.read(CONFIG_FILE)
+        return (json.loads(cfg["target"]["shops"]))
+
+
+
+    def get_last_json() -> dict:
+        _LAST_FLYERS_FILE = "last.json"
+
+        if os.path.exists(_LAST_FLYERS_FILE):
+            if os.path.getsize(_LAST_FLYERS_FILE) == 0:
+                ret = []
+            else:
+                json_open = open(_LAST_FLYERS_FILE, "r")
+                last = json.load(json_open)
+                ret = last["flyers"]
+        else:
+            ret = []
+
+        return ret
+
 
 def main():
-    Foo().say()
+    """
+    set slack token and target shops from config.ini
+    """
+    SLACK_BOT_TOKEN, SLACK_CHANNEL = F3.get_slack_conf()
+    TARGET_SHOPS = F3.get_target_shops()
+
     exit (0)
 
 
